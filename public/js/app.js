945bf6feb2a4ebb68fc82a9171bc3439ab31ee89
kwhitejr <kwhitejr.edu@gmail.com>
2015-12-23 19:47:45 -1000

    Header links working

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,39 @@
-$(function () {
-  $.ajax('/api/get_the_app.json', {
+
+$('#random').click(function () {
+  $.ajax('/api/random.json', {
     contentType: 'application/json',
-    success: getApp
+    success: getApi
   });
 });
 
-function getApp () {
+$('#myboards').click(function () {
+  $.ajax('/api/my_boards.json', {
+    contentType: 'application/json',
+    success: getApi
+  });
+});
+
+$('#gettheapp').click(function () {
+  $.ajax('/api/get_the_app.json', {
+    contentType: 'application/json',
+    success: getApi
+  });
+});
+
+function getApi (results) {
+  $('.story').remove();
   console.log('Connecting to API');
+  results.data.children.forEach(function(child) {
+    console.log(child.data.url);
+    var $div = $("<div />", {id: child.data.id, class: 'story'});
+    var $pic = $("<img />", {src: child.data.url});
+    var $title = $("<h1 />", {text: child.data.title});
+    var $author = $("<h2 />", {text: child.data.author});
+    var $time = $("<p />", {text: child.data.created});
+    var $description = $("<a />", {href: child.data.permalink});
+
+    $('#main').append(
+      $div.append($pic, $title, $author, $time, $description)
+    );
+  });
 }
