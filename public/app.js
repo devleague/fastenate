7f7deb76c36b699a7f44196deb84085f51508578
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-05 23:06:59 -1000

    starting to get json to appear on mainDiv still working on loading images though receiving a get error on img src path

--- public/app.js
+++ public/app.js
@@ -2,26 +2,45 @@
 //json is so don't have to refresh each time
 //simmilar to Uber exercise, grab data which is posted to html dynamically
 //make sure to link up jquery and app.js
+/*tutorial link on ajax using jquery
+https://www.youtube.com/watch?v=j-S5MBs4y0Q*/
 
-$(function(){
-  $.ajax('/api/get_the_app.json', {
-    contentType: 'application/json',
-    error: function(err){
-      console.error(err);
-    },
-    success: post
+// $("#random").click(function () {
+//   $.ajax("/api/random.json", {
+//   contentType: "application/json",
+//   success: getData
+//   });
+// });
 
+// $(function getData (data){
+//     console.log(data);
+//     console.log('hello');
+//   $(".containter").append("<p>" + data + "</p>");
+// });
+
+$(function () {
+  $.ajax('api/my_boards.json', {
+    success: getApp
   });
 });
 
+function getApp (boards){
+  var $mainDiv = $('.mainContainer');
+  var $myBoards = $('.myBoards');
+  // var $title = $('<h2 />');
+  //traversing the my_boards.json
+  //looked at how json file is setup, .data.children etc.
+  boards.data.children.forEach(function (board){
+    $myBoards.addClass('inContainer');
+    var srcPath = board.data.thumbnail;
+    // $title.text('Reload myBoards');
 
 
-function post (){
-  var $nav = $('<nav />');
+    var $listTitle = $('<li />').text(board.data.title);
+    var $img = $('<img src =' + srcPath +'/>');
 
-  $.getJSON('/api/get_the_app.json', function (data){
-    $(nav).atrr('/get-app').click(function (events){
-      $.each(data).appendTo(".block-four");
-    });
+    $myBoards.append($listTitle);
+    $myBoards.append($img);
   });
+  $mainDiv.append($myBoards);
 }
