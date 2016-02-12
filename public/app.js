a2af053d6c28aa35033f9df7ccf0bd2c6dbc7786
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-11 18:15:48 -1000

    two jsons posting to mainContainer working on click function to pull images into container objects centered and image class connected properly now with border-radius

--- public/app.js
+++ public/app.js
@@ -5,42 +5,68 @@
 /*tutorial link on ajax using jquery
 https://www.youtube.com/watch?v=j-S5MBs4y0Q*/
 
-// $("#random").click(function () {
-//   $.ajax("/api/random.json", {
-//   contentType: "application/json",
-//   success: getData
-//   });
-// });
-
-// $(function getData (data){
-//     console.log(data);
-//     console.log('hello');
-//   $(".containter").append("<p>" + data + "</p>");
-// });
+//TODOs: 1. add click function
+//       2. center items
+//       3. apply to each board, and make pretty
+//       4. how to make ajax more dry with boards? Areas that are changing, make into a var
+//       5. had to rename the success getApp function since was overriding
 
 $(function () {
-  $.ajax('api/my_boards.json', {
-    success: getApp
+  $('#clickRandom').click(function(){
+    console.log("in click function");
+    $.ajax('api/my_boards.json', {
+      success: getBoards
+    });
   });
 });
 
-function getApp (boards){
+function getBoards (boards){
+  var $titleTwo = $('<h2>' + 'Reload myBoards' + '</h2>');
   var $mainDiv = $('.mainContainer');
   var $myBoards = $('.myBoards');
-  // var $title = $('<h2 />');
-  //traversing the my_boards.json
-  //looked at how json file is setup, .data.children etc.
+
+  $myBoards.append($titleTwo);
+
   boards.data.children.forEach(function (board){
-    $myBoards.addClass('inContainer');
     var srcPath = board.data.thumbnail;
-    // $title.text('Reload myBoards');
-
-
     var $listTitle = $('<li />').text(board.data.title);
-    var $img = $('<img src =' + srcPath +'/>');
+    var $img = $('<img src =' + srcPath +'>');
 
+    $myBoards.addClass('inContainer');
+    $img.addClass('image');
     $myBoards.append($listTitle);
     $myBoards.append($img);
   });
   $mainDiv.append($myBoards);
 }
+
+
+$(function () {
+  $.ajax('api/random.json', {
+    success: getRandom
+  });
+});
+
+function getRandom (boards){
+  var $titleOne = $('<h2>' + 'Be Yourself .. Be Random' + '</h2>');
+  var $mainDiv = $('.mainContainer');
+  var $random = $('.random');
+
+  $random.append($titleOne);
+  //traversing the my_boards.json
+  //looked at how json file is setup, .data.children etc.
+  boards.data.children.forEach(function (board){
+    var srcPath = board.data.thumbnail;
+    var $listTitle = $('<li />').text(board.data.title);
+    var $img = $('<img src =' + srcPath +'>');
+
+    $random.addClass('inContainer');
+    $img.addClass('image');
+    $random.append($listTitle);
+    $random.append($img);
+  });
+  $mainDiv.append($random);
+}
+
+
+
